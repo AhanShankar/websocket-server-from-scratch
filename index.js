d80f5470be0b43071066c8631f8db136b5abecfd
AhanShankar <ahanshankar@gmail.com>
2024-12-22 11:51:01 +0530

    add basic validations

---
+++ index.js
@@ -0,0 +1,53 @@
+import http from "http";
+
+
+http.createServer(function (req, res) {
+    const url = req.url;
+    const method = req.method;
+    if (url == "/chat" && method == "GET") {
+        if(isValidHandshake(req)){
+            res.writeHead(101, {
+                'Upgrade': 'websocket',
+                'Connection': 'Upgrade',
+                'Sec-WebSocket-Accept': 's3pPLMBiTxaQ9kYGzzhZRbK+xOo='
+            });
+            res.end();
+        }
+        else
+        {
+            res.writeHead(400, { 'Content-Type': 'text/html' }); //write a response to the client
+            res.end(); //end the response
+            return;
+        }
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' }); //write a response to the client
+        res.end(); //end the response
+    }
+}).listen(8080); //the server object listens on port 8080
+
+/**
+ * Validates if the request is a valid handshake for a WebSocket connection
+ * @param {http.IncomingMessage} req the request from the client
+ * @returns {boolean} true if the request is a valid handshake, false otherwise
+ */
+
+function isValidHandshake(req) {
+    if (req.httpVersion === "1.0")
+        return false
+    if (!req.headers.host)
+        return false
+    if (req.headers.upgrade.toLowerCase() !== "websocket")
+        return false
+    if (req.headers.connection.toLowerCase() !== "upgrade")
+        return false
+    if (!req.headers["sec-websocket-key"])
+        return false
+    const decodedString = Buffer.from(req.headers["sec-websocket-key"], 'base64')
+    if (decodedString.length !== 16)
+        return false
+    // todo: return appropriate error code if the version is not supported
+    if (req.headers["sec-websocket-version"] !== "13")
+        return false
+    return true
+}
