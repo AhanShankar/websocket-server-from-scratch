9b85f4b28026390bbf66eed694bfddab029f7908
AhanShankar <ahanshankar@gmail.com>
2024-12-25 13:12:01 +0530

    add frame types to the file

--- ClientFrame.js
+++ ClientFrame.js
@@ -1,4 +1,12 @@
 // import { Frame } from "./frame";
+const FRAME_TYPES = {
+    CONTINUATION: 0,
+    TEXT: 1,
+    BINARY: 2,
+    CLOSE: 8,
+    PING: 9,
+    PONG: 10
+}
 
 export class ClientFrame {
 
